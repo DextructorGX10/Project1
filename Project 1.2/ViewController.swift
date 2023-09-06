499ec43da4b61a606a192a5421d37fb0de2946b0
DextructorGX10 <didipedrosa@hotmail.com>
2023-09-06 18:46:15 -0400

    Commit with layout of image good
    Signed-off-by: DextructorGX10 <didipedrosa@hotmail.com>

---
+++ Project 1.2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Project 1.2
+//
+//  Created by Diego Gama on 9/5/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    
+
+}
+
